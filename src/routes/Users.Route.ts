414cfa33189cfe74e833e3396d08611530b872d8
Wander95 <wanders1995@hotmail.com>
2020-10-15 19:18:21 -0400

    ✨ Add updated and delete functinoality
    ----> /user/:userId PUT
    ----> /user/:userId DELETE

--- src/routes/Users.Route.ts
+++ src/routes/Users.Route.ts
@@ -1,16 +1,20 @@
+import { Router } from 'express';
+
 import {
   createUser,
   getUsers,
   getOneUser,
+  deleteUser,
+  updateUser,
 } from '../controllers/User.Controller';
-import { Router } from 'express';
 
 const UserRouter: Router = Router();
 
 UserRouter.get('/user', getUsers);
 
 UserRouter.post('/user', createUser);
-// UserRouter.post('/:{id}', () => {});
+UserRouter.put('/user/:userId', updateUser);
+UserRouter.delete('/user/:userId', deleteUser);
 UserRouter.get('/user/:id', getOneUser);
 
 export default UserRouter;
