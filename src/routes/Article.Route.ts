2aef39593cf801464122924fd4f11864f4395e0f
Wander95 <wanders1995@hotmail.com>
2020-10-15 19:45:24 -0400

    ✨ Update, Delete articles / Comments
    ----> /articles/:articleId                      PUT
    ----> /articles/:articleId                      DELETE
    
    ----> /articles/:articleId/comments             POST
    ----> /articles/:articleId/comments             GET
    ----> /articles/:articleId/comments/:commentId  PUT
    ----> /articles/:articleId/comments/:commentId  DELETE
    
    ----> /comments                                 GET

--- src/routes/Article.Route.ts
+++ src/routes/Article.Route.ts
@@ -2,7 +2,18 @@ import {
   createArticle,
   getArticles,
   getOneArticle,
+  updateArticle,
+  deleteArticle,
 } from '../controllers/Article.Controller';
+
+import {
+  createComment,
+  getComments,
+  updateComment,
+  deleteComment,
+  getAllComments,
+} from '../controllers/Comment.Controller';
+
 import { Router } from 'express';
 
 const ArticlesRouter: Router = Router();
@@ -10,7 +21,17 @@ const ArticlesRouter: Router = Router();
 ArticlesRouter.get('/articles', getArticles);
 
 ArticlesRouter.post('/articles', createArticle);
-// UserRouter.post('/:{id}', () => {});
+ArticlesRouter.put('/articles/:articleId', updateArticle);
+ArticlesRouter.delete('/articles/:articleId', deleteArticle);
 ArticlesRouter.get('/articles/:id', getOneArticle);
 
+ArticlesRouter.post('/articles/:articleId/comments', createComment);
+ArticlesRouter.get('/articles/:articleId/comments', getComments);
+ArticlesRouter.put('/articles/:articleId/comments/:commentId', updateComment);
+ArticlesRouter.delete(
+  '/articles/:articleId/comments/:commentId',
+  deleteComment
+);
+
+ArticlesRouter.get('/comments', getAllComments);
 export default ArticlesRouter;
