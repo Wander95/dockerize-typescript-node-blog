22ddd89d45a95e79017451f90977828d607f9b1e
Wander95 <wanders1995@hotmail.com>
2020-11-02 22:33:20 -0400

    🎨 Protect routes

--- src/routes/Article.Route.ts
+++ src/routes/Article.Route.ts
@@ -14,22 +14,33 @@ import {
   getAllComments,
 } from '../controllers/Comment.Controller';
 
+import { isAuthorized } from '../libs/index';
+
 import { Router } from 'express';
 
 const ArticlesRouter: Router = Router();
 
 ArticlesRouter.get('/articles', getArticles);
 
-ArticlesRouter.post('/articles', createArticle);
-ArticlesRouter.put('/articles/:articleId', updateArticle);
-ArticlesRouter.delete('/articles/:articleId', deleteArticle);
+ArticlesRouter.post('/articles', isAuthorized, createArticle);
+ArticlesRouter.put('/articles/:articleId', isAuthorized, updateArticle);
+ArticlesRouter.delete('/articles/:articleId', isAuthorized, deleteArticle);
 ArticlesRouter.get('/articles/:id', getOneArticle);
 
-ArticlesRouter.post('/articles/:articleId/comments', createComment);
+ArticlesRouter.post(
+  '/articles/:articleId/comments',
+  isAuthorized,
+  createComment
+);
 ArticlesRouter.get('/articles/:articleId/comments', getComments);
-ArticlesRouter.put('/articles/:articleId/comments/:commentId', updateComment);
+ArticlesRouter.put(
+  '/articles/:articleId/comments/:commentId',
+  isAuthorized,
+  updateComment
+);
 ArticlesRouter.delete(
   '/articles/:articleId/comments/:commentId',
+  isAuthorized,
   deleteComment
 );
 
