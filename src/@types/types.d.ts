aa25825be9de56001f932d86c4cccb9e7be43847
Wander95 <wanders1995@hotmail.com>
2020-11-02 23:06:25 -0400

    ✨ Extend userId

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,5 +1,19 @@
-declare namespace Express {
-  export interface Request {
-    userId: string;
+import { IncomingHttpHeaders } from 'http';
+
+declare module 'http' {
+  interface IncomingHttpHeaders {
+    token: string;
+    KEY: string;
+  }
+}
+
+declare global {
+  namespace Express {
+    export interface Request {
+      userId: string;
+      authorization: {
+        token: string;
+      };
+    }
   }
 }
