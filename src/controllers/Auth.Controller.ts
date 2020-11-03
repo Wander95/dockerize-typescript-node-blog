0a4151ff21a8d96dd36f20d1bd6b88ef9f0616c7
Wander95 <wanders1995@hotmail.com>
2020-11-02 23:31:49 -0400

    ✨ Sync firebase user with mongo users ----------> Create users along with firebase auth ----------> Use of response API

--- src/controllers/Auth.Controller.ts
+++ src/controllers/Auth.Controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
-import firebaseConfig from '../firabase';
-
-const { admin, firebase } = firebaseConfig;
+import { success } from '../libs/ResponseAPI';
+import { firebase } from '../firebaseCon';
+import UserModel from '../models/User.Model';
 
 export const signIn = async (
   req: Request,
@@ -14,9 +14,8 @@ export const signIn = async (
     const currentUser = await firebase
       .auth()
       .signInWithEmailAndPassword(email, password);
-    return res.status(200).json({
-      currentUser,
-    });
+
+    return res.status(200).json(success('OK', res.statusCode, currentUser));
   } catch (error) {
     console.log('error.message', error.message);
     return res.status(400).json({
@@ -43,12 +42,23 @@ export const signUp = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const { email, password, name } = req.body;
+  const { email, password, name, lastName } = req.body;
 
   try {
-    const user = await firebase
+    const userCredentials = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password);
+
+    const newUser = new UserModel({
+      uid: userCredentials.user?.uid,
+      email,
+      password,
+      name,
+      lastName,
+    });
+
+    await newUser.save();
+
     // const hashedPassword = await bcrypt.hash(password, 10);
     // const authUser = await admin.auth().createUser({
     //   email,
@@ -60,7 +70,7 @@ export const signUp = async (
 
     return res.status(200).json({
       success: true,
-      user,
+      user: userCredentials,
     });
   } catch (error) {
     return res.status(400).json({
